9fe49743da7a93cf014758f783afe948a99df1eb
Mansi <mansikrish87@gmail.com>
2026-08-06 10:23:24 +0530

    Delete 4.4.sql

--- 4.4.sql
+++
@@ -1,7 +0,0 @@
--- Write a query to do the following:
-
--- FULL OUTER JOIN the 'student' and 'course' tables using 'Course_id' to match the tables. Output the joined table.
-
-select * from student
-full outer join course
-on student.Course_id=course.Course_id;
